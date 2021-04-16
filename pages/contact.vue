47f4b800b64efb5d134114d92634463353305d6b
Andy Beckmann <abec19@gmail.com>
2021-04-15 23:18:19 -0400

    Add contact page

---
+++ pages/contact.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="app--main">
+		<AppHeader />
+		<div class="app--body-contact">
+			<p>Contact</p>
+		</div>
+		<AppFooter />
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
